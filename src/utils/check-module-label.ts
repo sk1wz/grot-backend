2d531eabfa958ce7ac150a49879dc79e41198993
sk1wz <78929376+sk1wz@users.noreply.github.com>
2026-08-19 20:39:02 +0300

    feat: update

--- src/utils/check-module-label.ts
+++ src/utils/check-module-label.ts
@@ -2,7 +2,7 @@ import { CheckModuleEnums } from '@/db';
 
 const CHECK_MODULE_LABELS: Record<CheckModuleEnums, string> = {
   [CheckModuleEnums.GIBDD]: 'ГИБДД',
-  [CheckModuleEnums.LIMITATION]: 'Ограничения регистрации',
+  [CheckModuleEnums.LIMITATION]: 'Ограничения',
   [CheckModuleEnums.GISTORGI]: 'ГИС Торги',
   [CheckModuleEnums.FSSP]: 'ФССП',
   [CheckModuleEnums.BANKRUPTCY]: 'Банкротство',
